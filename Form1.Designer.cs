7ebad1da5634692b450b8cdc9f2833b1fe263bd1
Szymon <49693932+gitINPG@users.noreply.github.com>
2022-02-07 20:55:54 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace KEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBoxTrig = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.connectServerCtrl1 = new Opc.Ua.Client.Controls.ConnectServerCtrl();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 279);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(523, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBoxTrig
+            // 
+            this.textBoxTrig.Location = new System.Drawing.Point(389, 70);
+            this.textBoxTrig.Name = "textBoxTrig";
+            this.textBoxTrig.Size = new System.Drawing.Size(122, 20);
+            this.textBoxTrig.TabIndex = 7;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(261, 70);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(122, 20);
+            this.textBoxName.TabIndex = 10;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(133, 70);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(122, 20);
+            this.textBoxValue.TabIndex = 11;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(5, 70);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(122, 20);
+            this.textBoxID.TabIndex = 12;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // connectServerCtrl1
+            // 
+            this.connectServerCtrl1.Configuration = null;
+            this.connectServerCtrl1.DisableDomainCheck = false;
+            this.connectServerCtrl1.Location = new System.Drawing.Point(11, 12);
+            this.connectServerCtrl1.MaximumSize = new System.Drawing.Size(2048, 23);
+            this.connectServerCtrl1.MinimumSize = new System.Drawing.Size(500, 23);
+            this.connectServerCtrl1.Name = "connectServerCtrl1";
+            this.connectServerCtrl1.PreferredLocales = null;
+            this.connectServerCtrl1.ServerUrl = "";
+            this.connectServerCtrl1.SessionName = "Kep";
+            this.connectServerCtrl1.Size = new System.Drawing.Size(500, 23);
+            this.connectServerCtrl1.StatusStrip = this.statusStrip1;
+            this.connectServerCtrl1.TabIndex = 13;
+            this.connectServerCtrl1.UserIdentity = null;
+            this.connectServerCtrl1.UseSecurity = true;
+            this.connectServerCtrl1.ConnectComplete += new System.EventHandler(this.connectServerCtrl1_ConnectComplete);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 301);
+            this.Controls.Add(this.connectServerCtrl1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxTrig);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBoxTrig;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private Opc.Ua.Client.Controls.ConnectServerCtrl connectServerCtrl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Client.Controls;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Reflection;
+
+namespace KEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Session m_session;
+        private bool m_connectedOnce;
+        private Subscription m_subscription;
+        private IEncodeable val;
+
+        private void connectServerCtrl1_ConnectComplete(object sender, EventArgs e)
+        {
+            try
+            {
+                m_session = connectServerCtrl1.Session;
+
+                if (m_session != null && !m_connectedOnce)
+                {
+                    m_connectedOnce = true;
+                    CreateSubscriptionAndMonitorItem();
+                }
+
+            }
+            catch (Exception exception)
+            {
+                ClientUtils.HandleException(this.Text, exception);
+            }
+        }
+        private void CreateSubscriptionAndMonitorItem()
+        {
+            try
+            {
+                if (m_session == null)
+                {
+                    return;
+                }
+
+                if (m_subscription != null)
+                {
+                    m_session.RemoveSubscription(m_subscription);
+                    m_subscription = null;
+                }
+
+                m_subscription = new Subscription();
+                m_subscription.PublishingEnabled = true;
+                m_subscription.PublishingInterval = 1000;
+                m_subscription.Priority = 1;
+                m_subscription.KeepAliveCount = 10;
+                m_subscription.LifetimeCount = 20;
+                m_subscription.MaxNotificationsPerPublish = 1000;
+
+                m_session.AddSubscription(m_subscription);
+                m_subscription.Create();
+
+
+                MonitoredItem monitoredTrig = new MonitoredItem();
+                monitoredTrig.StartNodeId = new NodeId("OPC.PLC.Trig.SimPLC__1__Trig", 2);
+                monitoredTrig.AttributeId = Attributes.Value;
+                monitoredTrig.Notification += MonitoredItem_Notification;
+                m_subscription.AddItem(monitoredTrig);
+
+                MonitoredItem monitoredName = new MonitoredItem();
+                monitoredName.StartNodeId = new NodeId("OPC.PLC.Name.SimPLC__1__Name", 2);
+                monitoredName.AttributeId = Attributes.Value;
+                monitoredName.Notification += MonitoredItem_Notification;
+                m_subscription.AddItem(monitoredName);
+
+                MonitoredItem monitoredId = new MonitoredItem();
+                monitoredId.StartNodeId = new NodeId("OPC.PLC.Id.SimPLC__1__Id", 2);
+                monitoredId.AttributeId = Attributes.Value;
+                monitoredId.Notification += MonitoredItem_Notification;
+                m_subscription.AddItem(monitoredId);
+
+                MonitoredItem monitoredVal = new MonitoredItem();
+                monitoredVal.StartNodeId = new NodeId("OPC.PLC.IntValue.SimPLC__1__IntValue", 2);
+                monitoredVal.AttributeId = Attributes.Value;
+                monitoredVal.Notification += MonitoredItem_Notification;
+                m_subscription.AddItem(monitoredVal);
+
+                m_subscription.ApplyChanges();
+                
+            }
+            catch (Exception exception)
+            {
+                ClientUtils.HandleException(this.Text, exception);
+            }
+        }
+       
+        private void MonitoredItem_Notification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MonitoredItemNotificationEventHandler(MonitoredItem_Notification), monitoredItem, e);
+                return;
+            }
+            try
+            {
+                if (monitoredItem.StartNodeId.Identifier.ToString() == "OPC.PLC.Id.SimPLC__1__Id")
+                {
+                    textBoxID.Text = ((MonitoredItemNotification)e.NotificationValue).Value.ToString();
+                }
+                else if (monitoredItem.StartNodeId.Identifier.ToString() == "OPC.PLC.IntValue.SimPLC__1__IntValue")
+                {
+                    textBoxValue.Text = ((MonitoredItemNotification)e.NotificationValue).Value.ToString();
+                }
+                
+                
+            }
+            catch (Exception ex)
+            {
+                ClientUtils.HandleException(this.Text, ex);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.connectServerCtrl1.ServerUrl = "opc.tcp://127.0.0.1:49320";
+            string AppName = "Kep";
+            ApplicationConfiguration config = new ApplicationConfiguration()
+            {
+                ApplicationName = AppName,
+                ApplicationUri = Utils.Format(@"urn:{0}:" + AppName, System.Net.Dns.GetHostName()),
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    ApplicationCertificate = new CertificateIdentifier
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer",
+                        SubjectName = "CN=" + AppName + ", DC=" + System.Net.Dns.GetHostName()
+                    },
+                    TrustedIssuerCertificates = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer"
+                    },
+                    TrustedPeerCertificates = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\TrustedIssuer"
+                    },
+                    RejectedCertificateStore = new CertificateTrustList
+                    {
+                        StoreType = @"Directory",
+                        StorePath = System.Windows.Forms.Application.StartupPath + @"\Cert\RejectedCertificates"
+                    },
+                    AutoAcceptUntrustedCertificates = true,
+                    AddAppCertToTrustedStore = true,
+                    RejectSHA1SignedCertificates = false
+                },
+                TransportConfigurations = new TransportConfigurationCollection(),
+                TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+                ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                TraceConfiguration = new TraceConfiguration
+                {
+                    DeleteOnLoad = true
+                },
+                DisableHiResClock = false
+
+            };
+            config.Validate(ApplicationType.Client).GetAwaiter().GetResult();
+            if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += (s, ee) =>
+                { ee.Accept = (ee.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+            }
+
+            this.connectServerCtrl1.Configuration = config;
+            this.connectServerCtrl1.UserIdentity = new UserIdentity();
+            this.connectServerCtrl1.UseSecurity = true;
+
+            var application = new ApplicationInstance
+            {
+                ApplicationName = AppName,
+                ApplicationType = ApplicationType.Client,
+                ApplicationConfiguration = config
+
+            };
+            
+            Opc.Ua.Utils.SetTraceMask(0);//
+            application.CheckApplicationInstanceCertificate(true, 2048).GetAwaiter().GetResult();//create certificate
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.connectServerCtrl1.Disconnect();
+        }
+    }
+}
